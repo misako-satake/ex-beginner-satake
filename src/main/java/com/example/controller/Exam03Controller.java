1ebc9d03f863d22dd211999e5e23f0415a8fb145
misako-satake <misako.satake@rakus-partners.co.jp>
2021-11-02 09:16:44 +0900

    exam03のapplicationスコープ使用版

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,20 +13,24 @@ public class Exam03Controller {
 	
 	@RequestMapping("")
 	public String index() {
-		return "exam03";
+		return "exam03";		
 	}
 	
+	@Autowired
+	private ServletContext application;
+	
 	@RequestMapping("/result")
-	public String result(Integer obj1,Integer obj2,Integer obj3,Model model) {
+	public String result(Integer obj1,Integer obj2,Integer obj3) {
 		
-		int sum = obj1 + obj2 + obj3;
+		Integer sum = (Integer)(obj1 + obj2 + obj3);
 		int sum_tax = (int)(sum*1.1);
 		
-		model.addAttribute("obj1", obj1);
-		model.addAttribute("obj2", obj2);
-		model.addAttribute("obj3", obj3);
-		model.addAttribute("sum", sum);
-		model.addAttribute("sum_tax", sum_tax);
+		
+
+		
+		application.setAttribute("sum", sum);
+		application.setAttribute("sum_tax", sum_tax);
+		
 		
 		return "exam03-result";
 	}
